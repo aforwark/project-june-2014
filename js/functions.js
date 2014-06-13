6cd6b589357c65bd0dd957e71b392485823c7811
Andy Forwark <andrew.forwark@sheknows.com>
2014-06-12 22:07:16 -0700

    Finishing up the carousel and other fine tuning

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,14 @@ $(window).load(function(){
         topSpacing: 0, 
         getWidthFrom: '#navigation' 
     });
+    $('.dropdown li ul').hide().removeClass('fallback');
+    $('.dropdown li').hover(
+      function () {
+        $('ul', this).stop().slideDown(100);
+      },
+      function () {
+        $('ul', this).stop().slideUp(100);
+      }
+    );
+    $('.carousel').carousel()
 });
